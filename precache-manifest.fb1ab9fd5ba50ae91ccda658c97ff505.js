f51dbedc41d427a2a4f4120ee5f64e42abfcd695
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-05-02 16:18:04 +0200

    Updates

---
+++ precache-manifest.fb1ab9fd5ba50ae91ccda658c97ff505.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d39a5e3cfbacbcfff2722bcf6f83f02",
+    "url": "/pokehooks/index.html"
+  },
+  {
+    "revision": "097fddf4f5c5c4257b49",
+    "url": "/pokehooks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "02672c7d3d5f351d6545",
+    "url": "/pokehooks/static/js/2.54aea067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokehooks/static/js/2.54aea067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "097fddf4f5c5c4257b49",
+    "url": "/pokehooks/static/js/main.f1e2ad14.chunk.js"
+  },
+  {
+    "revision": "9dfded2a76d77bbe3dc6",
+    "url": "/pokehooks/static/js/runtime-main.1502c45f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokehooks/static/media/logo.5d5d9eef.svg"
+  }
+]);
