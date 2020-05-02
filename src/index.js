a10adbdb7c8aca8c6489236c7401707aa69798ad
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-05-02 19:24:35 +0200

    add hooks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import "./index.css";
+
+function App() {
+  const [pokedex, setPokedex] = useState([]);
+  const [wildPokemon, setWildPokemon] = useState({});
+
+  useEffect(() => {
+    encounterWildPokemon();
+    // eslint-disable-next-line
+  }, []);
+
+  const pokeId = () => {
+    const min = Math.ceil(1);
+    const max = Math.floor(151);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const encounterWildPokemon = () => {
+    const API_URL = "https://pokeapi.co/api/v2/pokemon/" + pokeId();
+    axios.get(API_URL).then((response) => {
+      setWildPokemon(response.data);
+    });
+  };
+
+  return (
+    <div className="app-wrapper">
+      <header>
+        <h1 className="title">Pokehooks</h1>
+
+        <section className="wild-pokemon">{wildPokemon.name}</section>
+      </header>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
